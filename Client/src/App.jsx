23a2970e4a82d5cbb5e9f1d5907d18b14fffa112
Praveen Sharma <praying.sharma@gmail.com>
2025-07-23 18:17:29 +0530

    Admin Login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/admin/Dashboard";
 import AddBlog from "./pages/admin/AddBlog";
 import ListBlog from "./pages/admin/ListBlog";
 import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blog/:id" element={<BlogPage />} />
-          <Route path="/admin" element={<Layout />}> 
+          <Route path="/admin" element={false ?   <Layout /> : <Login />}> 
             <Route index element={<Dashboard />} />
             <Route path="addBlog" element={<AddBlog />} />
             <Route path="listBlog" element={<ListBlog />} />
