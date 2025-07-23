e1124d6422a4fe57c3f837150315bee8f8d1200c
Praveen Sharma <praying.sharma@gmail.com>
2025-07-23 10:52:09 +0530

    Blog Page

--- Client/src/pages/BlogPage.jsx
+++ Client/src/pages/BlogPage.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { blog_data } from "../assets/assets";
 
 function BlogPage() {
-  return (
+  const { id } = useParams();
+  console.log("id", id);
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchBlogData = async () => {
+    try {
+      const data = blog_data.find((item) => item._id === id);
+      setData(data);
+      console.log("Blog Data is", data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, [id]);
+
+  if (error) {
+    return <h1>Error comes</h1>;
+  }
+
+  return data ? (
     <div>
       <h1>Blog Page</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>Loading...</h1>
     </div>
   );
 }
