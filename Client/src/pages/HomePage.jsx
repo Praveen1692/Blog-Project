cf0c508ea0ba2ce4deaff64418322f49bc932148
Praveen Sharma <praying.sharma@gmail.com>
2025-07-24 09:30:54 +0530

    Dashboard

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import BlogList from "../components/BlogList";
 import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
 
 function HomePage() {
   return (
--- Client/src/pages/admin/Dashboard.jsx
+++ Client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { assets, dashboard_data } from "../../assets/assets";
 
 function Dashboard() {
+  const [dashboardData, setDashboardData] = useState({
+    blogs: 0,
+    comments: 0,
+    drafts: 0,
+    recentBlogs: [],
+  });
+
+  const fetchDashboard = async () => {
+    setDashboardData(dashboard_data);
+  };
+
+  useEffect(() => {
+    fetchDashboard;
+  }, []);
   return (
-    <div>Dashboard</div>
-  )
+    <div className="flex-1 p-4 md:p-10 bg-blue-50/50">
+      <div className="flex flex-wrap gap-4">
+        <div>
+          <img src={assets.dashboard_icon_1} alt="Dash" />
+
+          <div>
+            <p>{}das</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
