8b979c248d61e22477086742ad1b68f7352820c8
Praveen Sharma <praying.sharma@gmail.com>
2025-07-31 10:26:49 +0530

    Add Blog

--- Client/src/pages/admin/AddBlog.jsx
+++ Client/src/pages/admin/AddBlog.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 function AddBlog() {
+  const [image, setImage] = useState(null);
   return (
-    <div>AddBlog</div>
-  )
+    <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+        <p>Upload thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={assets.upload_area}
+            alt="upload_image"
+            className="mt-2 h-16 rounded cursor-pointer "
+          />
+          <input type="file" id="image" hidden required />
+        </label>
+      </div>
+    </form>
+  );
 }
 
-export default AddBlog
+export default AddBlog;
--- Client/src/pages/admin/Comments.jsx
+++ Client/src/pages/admin/Comments.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { comments_data } from "../../assets/assets";
+import CommentTableItem from "../../components/admin/CommentTableItem";
 
 function Comments() {
   const [comments, setComments] = useState([]);
@@ -57,9 +58,20 @@ function Comments() {
             </tr>
           </thead>
 
-
           <tbody>
-            
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => (
+                <CommentTableItem
+                  key={comment._id}
+                  comment={comment}
+                  index={index + 1}
+                  fetchComments={fetchComments}
+                />
+              ))}
           </tbody>
         </table>
       </div>
