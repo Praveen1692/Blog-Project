1df178b4ce5f5e424e025c50b9c698a685973dd0
Praveen Sharma <praying.sharma@gmail.com>
2025-07-31 12:50:53 +0530

    Server

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,20 @@
-console.log("Server");
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
