4deb1f6593d536809a8f1a032831481b9bc82f51
Praveen Sharma <praying.sharma@gmail.com>
2025-07-31 11:40:25 +0530

    Add Blog

--- Client/src/pages/admin/AddBlog.jsx
+++ Client/src/pages/admin/AddBlog.jsx
@@ -95,15 +95,23 @@ function AddBlog() {
         </select>
 
         {/* Button */}
-        <div className=" mt-5 p-4">
+        <div className=" mt-5 flex gap-4">
           <p>Publish Now</p>
           <input
-            type="chechbox"
+            type="checkbox"
             name="checkbox"
             checked={isPublished}
             className="scale-125 cursor-pointer"
+            onChange={(e) => setIsPublished(e.target.checked)}
           />
         </div>
+
+        <button
+          type="submit"
+          className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm"
+        >
+          Add Blog
+        </button>
       </div>
     </form>
   );
