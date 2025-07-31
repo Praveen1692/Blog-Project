635be646f9fb6ed57749cf54056dab6121e909f9
Praveen Sharma <praying.sharma@gmail.com>
2025-07-31 13:34:20 +0530

    Admin Controller

---
+++ Server/controllers/adminController.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    if (
+      email !== process.env.ADMIN_EMAIL ||
+      password !== process.env.ADMIN_PASSWORD
+    ) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = await jwt.sign({ email }, process.env.JWT_SECRET);
+    console.log("Admin Token", token);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
