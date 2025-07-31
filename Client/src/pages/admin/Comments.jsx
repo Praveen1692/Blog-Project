9a772b25c5408d48285e9e0fd7024b456bb4f4b3
Praveen Sharma <praying.sharma@gmail.com>
2025-07-31 09:22:48 +0530

    Comments

--- Client/src/pages/admin/Comments.jsx
+++ Client/src/pages/admin/Comments.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { comments_data } from "../../assets/assets";
 
 function Comments() {
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState("Not Approved");
+
+  const fetchComments = async () => {
+    setComments(comments_data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
   return (
-    <div>Comments</div>
-  )
+    <div className="flex-1 pt-5 px-5 sm:Pt-12 sm:pl-16 bg-blue-50/50">
+      <div className="flex justify-between items-center max-w-3xl">
+        <h1>Comments</h1>
+
+        <div className="flex gap-4">
+          <button
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs   ${
+              filter === "Approved" ? "text-primary" : "text-gray-700"
+            }`}
+          >
+            Approved
+          </button>
+
+
+           <button
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs   ${
+              filter === "Approved" ? "text-primary" : "text-gray-700"
+            }`}
+          >
+            Approved
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Comments
+export default Comments;
