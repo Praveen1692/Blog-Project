72da83a48cc98ae17c862627f4eff7ade35d04ac
Praveen Sharma <praying.sharma@gmail.com>
2025-08-01 15:42:09 +0530

    Admin Routes

--- Server/routes/adminRoutes.js
+++ Server/routes/adminRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   adminLogin,
+  approveCommentById,
   deleteCommentById,
   getAllBlogsAdmin,
   getAllComments,
+  getDashboard,
 } from "../controllers/adminController.js";
 import auth from "../middleware/auth.js";
 
@@ -14,5 +16,8 @@ adminRouter.post("/login", adminLogin);
 adminRouter.get("/comments", auth, getAllComments);
 adminRouter.get("/blogs", auth, getAllBlogsAdmin);
 adminRouter.post("/delete-comment", auth, deleteCommentById);
+adminRouter.post("/approve-comment", auth, approveCommentById);
+
+adminRouter.get("/dashboard", auth, getDashboard);
 
 export default adminRouter;
